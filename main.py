65f5b3e62120b5ce250d8ba41784122b14dd1338
Brahm Capoor <brahm@stanford.edu>
2016-05-15 20:18:07 -0700

    Shifted to using xkcd json
    Shifted to using xkcd-provided json

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import requests
 import os
 
 def download_newest_post():
-    page = requests.get("http://www.xkcd.com").text
-    soup = BeautifulSoup(page, "html.parser")
-    imagelink = soup.find(id="comic")
-    img = imagelink.find("img")
-    img = "http:" + img['src']
-    img = requests.get(img)
+    comic = requests.get("http://xkcd.com/info.0.json").json()
+    img = requests.get(comic['img'])
     with open("img.png", 'wb') as pic:
         for chunk in img.iter_content(4096):
             pic.write(chunk)
