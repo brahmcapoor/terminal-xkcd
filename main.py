f08d011ec9baab92d9210e7c4eb7c045b3486340
Brahm Capoor <brahm@stanford.edu>
2016-05-17 00:24:02 -0700

    Formatted to comply with PEP8

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import os
 import pickle
 from tqdm import tqdm
 
+
 class xkcdcomic():
+
     def __init__(self, json):
         self.title = json['title']
         self.number = int(json['num'])
         self.alt = json['alt']
         self.transcript = json['transcript']
         self.link = json['img']
+
     def __str__(self):
         return(("{}: {}").format(self.title, self.link))
 
@@ -19,7 +22,7 @@ def load_database():
     database = {}
     with open('xkcd_database.pickle', 'rb') as f:
         database = pickle.load(f)
-    #get number of newest comic
+    # get number of newest comic
     newest_num = requests.get("http://xkcd.com/info.0.json").json()['num']
     print(len(database))
     if(len(database) + 1 == newest_num):
@@ -28,23 +31,26 @@ def load_database():
     else:
         print("Generating database...")
         for i in tqdm(range(len(database), newest_num + 1)):
-            #create database of comics to search through
+            # create database of comics to search through
             num = str(i)
             url = ("http://xkcd.com/{}/info.0.json").format(num)
             try:
                 jsondata = requests.get(url).json()
                 comic = xkcdcomic(jsondata)
                 database[comic.number] = comic
-            except: continue
+            except:
+                continue
         with open('xkcd_database.pickle', 'wb') as f:
-            #save database to file
-            pickle.dump(database,f)
+            # save database to file
+            pickle.dump(database, f)
         return database
+
 
 def download_comic(img):
     with open("img.png", 'wb') as pic:
         for chunk in img.iter_content(4096):
             pic.write(chunk)
+
 
 def show_prompt(comic):
     print(("Comic title: {}").format(comic.title))
@@ -58,7 +64,11 @@ def show_prompt(comic):
 def main():
     database = load_database()
     number_comics = str(len(database) + 1)
-    number = int(input("Comic number? The most recent comic is number " + number_comics + ". "))
+    number = int(
+        input(
+            "Comic number? The most recent comic is number " +
+            number_comics +
+            ". "))
     comic = database[number]
     url = comic.link
     img = requests.get(url)
